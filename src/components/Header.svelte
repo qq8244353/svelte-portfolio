30734354cb5011c62449f325b33683d4910ee6ae
qq8244353 <qqq000a@outlook.com>
2024-06-21 14:45:13 +0900

    add header

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<header>
+  <div>
+    <span> qq8244353's blog</span>
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<style>
+  header{
+    background: green;
+  }
+  div{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  ul{
+    display: flex;
+    justify-content: space-around;
+    list-style-type: none;
+  }
+  li{
+    width: 100%;
+    margin: 0em 2em;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Header from '../components/Header.svelte'
+</script>
+
+<Header/>
+<main>
+  <h1>Welcome to qq8244353's blog</h1>
+</main>
+
+<style>
+  h1 {
+    color: white;
+  }
+  main {
+    display: flex;
+  }
+</style>
